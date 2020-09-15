5104adf1fab18e461063e8c00590938ade820c5e
zmora <zmora.off@gmail.com>
2020-09-15 13:50:19 +0300

    1.0: 60+%; chunks;

--- README.md
+++ README.md
@@ -1,2 +1,28 @@
 # Spotify music transfer app
-# Search works **40%**
+### PIP
+```shell
+pip install spotipy
+pip install colorama
+```
+### Create app [here](https://developer.spotify.com/dashboard/applications)
+Create app and copy CLIENT ID, CLIENT SECRET, REDIRECT URI
+### Environment
+```shell
+export SPOTIPY_CLIENT_ID='1234567...xyz'
+export SPOTIPY_CLIENT_SECRET='1234567...xyz'
+export SPOTIPY_REDIRECT_URI='http://localhost:8080'
+```
+### Run **app.py**
+```shell
+python app.py DATA.FILE PLAYLIST.NAME
+```
+DATA.FILE:
+```
+...
+Terminal Function - Ablation Renewed
+Meshuggah - Bleed
+Sam Lee & Daniel Pemberton - The Devil & the Huntsman
+Findlay - Off & On
+Lamb of God - Walk with Me In Hell
+...
+```
--- app.py
+++ app.py
@@ -1,88 +1,125 @@
 import os
 import re
 import spotipy
-import time
-import random
 
-from spotipy.oauth2 import SpotifyOAuth
-from spotipy.oauth2 import SpotifyClientCredentials
+from colorama import Fore, Style
 
 
 def clean_data(line):
 	""""""
-	pattern = r'([\[\(][^\]\)]{0,}[\]\)])'
-	line = re.sub(pattern, '', line)
+	pattern1 = r'([\[\(][^\]\)]{0,}[\]\)])'
+	pattern2 = r'[\(\[].{0,}'
+	line = re.sub(pattern1, '', line)
+	line = re.sub(pattern2, '', line)
 	lines = line.split('-', 1)
 	lines = [l.strip() for l in lines]
-	return f'{lines[0]} - {lines[1]}'
+	return lines
+
+
+def lazy_search(track, spoti):
+	""""""
+	result = spoti.search(f'{track[0]} - {track[1]}', limit=1, type='track')
+	if result['tracks']['items']:
+		return result['tracks']['items'][0]['uri']
+	return None
+
+
+def semihard_search(track, spoti):
+	""""""
+	result = spoti.search(track[1], limit=50, type='track')
+	items = result['tracks']['items']
+	if not items: return None
+	for item in items:
+		if not item: continue #None иногда прилетает от spotify
+		if track[0].lower() == item['artists'][0]['name'].lower():
+			return item['uri']
+	return None
 
 
 def search_from_file(tracklist, spoti):
 	""""""
-	count = {'hit': 0, 'miss': 0}
-	missed = []
-	with open(tracklist, 'r') as file, open('track_uris.txt', 'w') as temp_uri_list:
+	hit, miss, missed = 0, 0, []
+	with open(tracklist, 'r') as file, open('track_uris', 'w') as temp_uri_list:
 		for n, track in enumerate(file.readlines()):
 			clean_track = clean_data(track)
-			result = spoti.search(clean_track, limit=1, type='track')
-			if result['tracks']['items']:
-				temp_uri_list.write(result['tracks']['items'][0]['uri']+'\n')
-				count['hit']+=1
-				print(f'\t[{n:^4}][  OK  ] "{clean_track}" ')
+			result = lazy_search(clean_track, spoti)
+			if not result:
+				result = semihard_search(clean_track, spoti)
+			if result:
+				temp_uri_list.write(result + '\n')
+				hit+=1
+				print(Fore.GREEN + f'\t[{n:^4}][  OK  ] "{clean_track[0]} - {clean_track[1]}"')
 			else:
 				missed.append(track)
-				count['miss']+=1
-				print(f'\t[{n:^4}][  NF  ] "{clean_track}" ')
-	with open('missed.txt', 'w') as missed_file:
+				miss+=1
+				print(Fore.RED + f'\t[{n:^4}][  NF  ] "{clean_track[0]} - {clean_track[1]}"')	
+	with open('missed', 'w') as missed_file:
 		for item in missed:
 			missed_file.write(item)
-	search_from_file.count = count
+	search_from_file.count = {'hit': hit, 'miss': miss}
 
 
 def add_to_playlist(playlist_uri, spoti):
 	""""""
 	try:
-		with open('track_uris.txt', 'r') as temp_uri_list:
+		with open('track_uris', 'r') as temp_uri_list:
 			track_uris = [line.strip() for line in temp_uri_list.readlines()]
-
-
-			for track_uri in track_uris:
-				test_res = spoti.playlist_add_items(playlist_uri, [track_uri,])
-	except FileNotFoundError as e1:
-		print('[FileNotFound]')
-		print(e1)
-	except spotipy.exceptions.SpotifyException as e2:
-		print('[!!!!!!!!!!!!!!]')
-		print(e2.msg)
+			tq = tqg(track_uris, N=50)
+			for i, t in enumerate(tq):
+				r = spoti.playlist_add_items(playlist_uri, t)
+				ccount = i
+			tcount = len(track_uris)
+	except Exception as e:
+		print('\t[FUCK] ', e.msg)
 	finally:
-		os.remove('track_uris.txt')
-		
+		os.remove('track_uris')
+		add_to_playlist.added = {'tracks':tcount, 'chunks':ccount}
+
+
+def tqg(tlist, N=10):
+	""""""
+	L = list(tlist)
+	while True:
+		if len(L) > N:
+			yield L[:N]
+			del L[:N]
+		else:
+			yield L
+			return []
+
 
 def authorization():
 	""""""
 	scope = 'playlist-modify-public'
-	spoti = spotipy.Spotify(auth_manager=SpotifyOAuth(scope=scope))
+	spoti = spotipy.Spotify(auth_manager=spotipy.oauth2.SpotifyOAuth(scope=scope))
 	user_id = spoti.me()['id']
 	return user_id, spoti
 
 
 def main(tracklist, playlist):
 	""""""
-
+	#
 	print('[] Authorization')
 	user_id, spoti = authorization()
-
+	#
 	print(f'[] Search from {tracklist}')
 	search_from_file(tracklist, spoti)
-
+	#
 	rate = search_from_file.count
-	print(f'[] Results: {rate}')
-
+	count = rate['hit'] + rate['miss']
+	p = int(rate['hit'] * 100 / count)
+	print(Style.RESET_ALL)
+	print(f'[  Found: {p}%   {rate["hit"]} of  {count}  ]')
+	#
 	print(f'[] Create playlist {playlist}')
 	playlist = spoti.user_playlist_create(user_id, playlist)
-
+	#
 	print('[] Add tracks to playlist')
 	add_to_playlist(playlist['uri'], spoti)
+	#
+	added = add_to_playlist.added
+	print(f'[  Added: {added["tracks"]} tracks in  {added["chunks"]} chunks  ]')
+
 
 if __name__ == '__main__':
 	import sys
@@ -90,5 +127,3 @@ if __name__ == '__main__':
 		main(sys.argv[1], sys.argv[2])
 	else:
 		print ('Please provide a tracklist file and playlist name!')
-
-	
